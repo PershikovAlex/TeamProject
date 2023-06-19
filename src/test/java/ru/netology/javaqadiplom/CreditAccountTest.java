7b671566bad912de22a464f45d389cc0b57ab836
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-06-19 18:38:04 +0500

    Add test1

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -7,14 +7,19 @@ public class CreditAccountTest {
 
     @Test
     public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
+        CreditAccount account = new CreditAccount(0, 5_000, 15);
 
         account.add(3_000);
 
         Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void negativeBalanceAfterAmount() {
+        CreditAccount account = new CreditAccount(2_000, 10_000, 15);
+
+        account.pay(10_000);
+
+        Assertions.assertEquals(-8_000, account.getBalance());
     }
 }
